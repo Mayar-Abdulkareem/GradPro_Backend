35967b550f3bb28d75977e2215327dd6b27f93b6
Mayar-Abdulkareem <fts@Mayar-Abdulkareem.local>
2023-12-18 14:31:03 +0200

    test

--- previousProjectsRouter.js
+++ previousProjectsRouter.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
-const tokenVerification = require("./tokenVerification");
 
 const previousProjectSchema = new mongoose.Schema({
   name: String,
@@ -10,6 +9,7 @@ const previousProjectSchema = new mongoose.Schema({
   students: String,
   supervisor: String,
   description: String,
+  link: String,
 });
 
 const PreviousProject = mongoose.model(
@@ -17,14 +17,55 @@ const PreviousProject = mongoose.model(
   previousProjectSchema
 );
 
-router.get("/previousProjects", async (req, res) => {
-  try {
-    const previousProject = await PreviousProject.find({});
+const PAGE_SIZE = 5;
 
-    if (!previousProject) {
-      res.status(404).json("No Previous Projects To Show");
+router.post("/previousProjects", async (req, res) => {
+  try {
+    const page = parseInt(req.body.page) || 1;
+    const skip = (page - 1) * PAGE_SIZE;
+
+    const filter = {};
+
+    // Add filters based on request body
+    if (req.body.projectName) {
+      filter.name = { $regex: new RegExp(req.body.projectName, "i") }; // Case-insensitive regex match
+    }
+
+    if (req.body.projectType.length > 0) {
+      filter.projectType = req.body.projectType;
+    }
+
+    const sort = {};
+
+    // Add sorting based on request body
+    if (req.body.sortByDate) {
+      sort.date = req.body.sortByDate === "asc" ? 1 : -1; // 1 for ascending, -1 for descending
+    }
+
+    const [previousProjects, totalCount] = await Promise.all([
+      PreviousProject.find(filter)
+        .collation({ locale: "en_US", numericOrdering: true })
+        .sort(sort)
+        .skip(skip)
+        .limit(PAGE_SIZE),
+      PreviousProject.countDocuments(filter),
+    ]);
+
+    if (!previousProjects || previousProjects.length === 0) {
+      res.json({
+        totalCount: 0,
+        currentPage: page,
+        pageSize: PAGE_SIZE,
+        test: "test",
+        previousProjects: [], // Return an empty array
+      });
     } else {
-      res.json(previousProject);
+      res.json({
+        totalCount: totalCount,
+        currentPage: page,
+        pageSize: PAGE_SIZE,
+        previousProjects: previousProjects,
+      });
     }
   } catch (error) {
     console.error("Error during fetching previous projects:", error);
